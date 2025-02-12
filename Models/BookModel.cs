8affc64e81e0c0f879483ce832ba07cec027531f
julieandersson <juan2301@student.miun.se>
2025-02-12 15:23:37 +0200

    lägger in sqlite databasfiler som db-shm och db-wal i gitignore, ändrar i CRUD-filerna för boksamlingen till svensk text

--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace moment3.Models {
     public class BookModel {
         // Properties
         public int Id { get; set; }
+        [Display(Name = "Titel")]
         public string? Title { get; set; }
+        [Display(Name = "Författare")]
         public string? Author { get; set; }
+        [Display(Name = "Beskrivning")]
         public string? Description { get; set; }
+        [Display(Name = "Publiceringsår")]
         public int PublishedYear { get; set; }
     }
 }
