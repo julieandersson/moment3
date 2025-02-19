b4d49ec5afe25ccd18e5bda2cd4118bdf8ae52ce
julieandersson <juan2301@student.miun.se>
2025-02-19 13:26:43 +0200

    skapar egen felhantering i formulären för bok och författare, sätter alla fält som obligatoriska

--- Models/AuthorModel.cs
+++ Models/AuthorModel.cs
@@ -5,6 +5,7 @@ namespace moment3.Models {
         // Properties
         public int Id { get; set; }
         
+        [Required(ErrorMessage = "Du måste ange författarens namn.")]
         [Display(Name = "Namn")]
         public string? Name { get; set; }
 
--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -5,15 +5,20 @@ namespace moment3.Models {
         // Properties
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Du måste ange en titel på boken.")]
         [Display(Name = "Titel")]
         public string? Title { get; set; }
 
+        [Required(ErrorMessage = "Du måste ange en beskrivning av boken.")]
         [Display(Name = "Beskrivning")]
         public string? Description { get; set; }
 
+        [Required(ErrorMessage = "Du måste ange bokens publiceringsår.")]
+        [Range(1000, 9999, ErrorMessage = "Publiceringsår måste vara ett giltigt årtal.")]
         [Display(Name = "Publiceringsår")]
         public int PublishedYear { get; set; }
 
+        [Required(ErrorMessage = "Du måste välja en författare i listan.")]
         [Display(Name = "Författare")]
         public int? AuthorModelId { get; set; }
 
